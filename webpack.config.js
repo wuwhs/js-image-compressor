243c2fda94e682923f21e6d67a2967a4402906e0
wuwhs <605776026@qq.com>
2020-07-30 18:46:27 +0800

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const TerserWebpackPlugin = require('terser-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+  mode: 'none',
+  entry: {
+    'image-compressor': './src/index.js',
+    'image-compressor.min': './src/index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].js',
+    library: 'ImageCompressor',
+    libraryExport: 'default',
+    libraryTarget: 'umd'
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserWebpackPlugin({
+        include: /\.min\./,
+      })
+    ]
+  }
+}
