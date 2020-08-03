424912519d05852281b3b691cc8e54c2e6c386b1
wuwhs <605776026@qq.com>
2020-08-03 23:09:49 +0800

    fix path error

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./dist/index.min.js')
+  module.exports = require('./dist/image-compressor.min.js')
 } else {
-  module.exports = require('./dist/index.js')
+  module.exports = require('./dist/image-compressor.js')
 }
